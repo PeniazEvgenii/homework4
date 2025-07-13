f3285a027e5a663566d9a463a5a9a17f88ff2350
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2025-07-13 13:33:50 +0300

    TASK | sql scripts

---
+++ homework5/sql/01_ddl.sql
@@ -0,0 +1,18 @@
+CREATE TABLE app.users
+(
+    id uuid,
+    email character varying(128) NOT NULL,
+    firstname character varying(64) NOT NULL,
+    lastname character varying(64) NOT NULL,
+    birth_date date NOT NULL,
+    gender character varying(16) NOT NULL,
+    password character varying(64) NOT NULL,
+    discount numeric(3, 1),
+    dt_create timestamp with time zone NOT NULL,
+    dt_update timestamp with time zone NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id),
+    CONSTRAINT users_email_unique UNIQUE (email)
+);
+
+ALTER TABLE IF EXISTS app.users
+    OWNER to postgres;
---
+++ homework5/sql/02_ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE app.files
+(
+    id uuid,
+    status character varying(10) NOT NULL,
+    dt_create timestamp with time zone NOT NULL,
+    dt_update timestamp with time zone NOT NULL,
+    CONSTRAINT files_pk PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS app.files
+    OWNER to postgres;
